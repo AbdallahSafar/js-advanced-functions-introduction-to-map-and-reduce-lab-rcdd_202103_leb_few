ee5b64f9c2ac606e824a343f2b03230fbe95ea24
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-27 15:38:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ function mapToSquare(sourceArray) {
 function reduceToTotal(sourceArray, startingPoint) {
   return sourceArray.reduce((total,item,startingPoint) => {
     total -= item;
-    return total;
+    return total
   }
 }
