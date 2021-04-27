4f0fbd5e1fcc81dab990bf09acf796735c884b04
Abdallah Safar <abdallah.safar01@gmail.com>
2021-04-27 15:28:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ function mapToNegativize(sourceArray) {
   return sourceArray.map(item => {
     item *= -1;
     return item;
-  })
+  });
 }
 
 function mapToNoChange(sourceArray) {
-
+  return sourceArray.map(item => {
+    return item;
+  });
 }
